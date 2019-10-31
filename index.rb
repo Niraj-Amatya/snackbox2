5d64805323b782edd481e1652502060623a9281a
Niraj <amatyaraj@gmail.com>
2019-10-31 19:35:59 +1100

    unfav food added

--- index.rb
+++ index.rb
@@ -1,5 +1,10 @@
 fav_food = ["momo", "pizza", "sandwich"]
+unfav_food = ["apple", "veggies"]
 
 for foods in fav_food
     puts "I like #{foods}."
+end
+
+for unfav in unfav_food
+    puts "My unfav food #{unfav}"
 end
